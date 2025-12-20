ef9a6c0485f3ae1cda1c875d1e8d5fb7f47bffd7
Mehran Mahmoudi <mehranmahmoudi082@gmail.com>
2025-12-20 16:05:24 +0330

    day 14: arrays, loops, map/filter/reduce

---
+++ month2/day14-arrays-loops.js
@@ -0,0 +1,36 @@
+// Movies Array
+
+const movies = [
+  { title: "Inception", rating: 8.8, genre: "Sci-Fiction" },
+  { title: "The Matrix", rating: 8.7, genre: "Action" },
+  { title: "Interstellar", rating: 8.6, genre: "Sci-Fiction" },
+  { title: "The Shawshank Redemption", rating: 9.3, genre: "Drama" },
+  { title: "Pulp Fiction", rating: 8.9, genre: "Crime" },
+];
+
+console.log("All Movies: ");
+movies.forEach((movie, index) => {
+  console.log(`${index + 1}. ${movie.title} - Rating: ${movies.rating}`);
+});
+
+// Movie IMBD Higher Than 9
+
+const highRated = movies.filter((movie) => movie.rating > 9);
+console.log(
+  "Movies IMDB Higher Than 9: \n",
+  highRated.map((m) => m.title)
+);
+
+// SCI-Fiction Movies
+
+const sciFi = movies.filter((movie) => movie.genre === "Sci-Fiction");
+console.log(
+  "Sci-Fiction Movies\n",
+  sciFi.map((m) => m.title)
+);
+
+// Average Movie IMDB
+
+const averageRating =
+  movies.reduce((sum, movie) => sum + movie.rating, 0) / movies.length;
+console.log("Average IMDB:\n", averageRating.toFixed(2));
